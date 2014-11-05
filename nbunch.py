75e76fbffff7dc2dcd83f6c71484e06a2e74e748
Ralph-Wang <ralph_ld@aliyun.com>
2014-11-05 16:52:35 +0800

    void 0

--- nbunch.py
+++ nbunch.py
@@ -4,15 +4,20 @@
 
 class NBunch(dict):
 
-    def __init__(self, *args, **kwargs):
+    def __init__(self, default=None, *args, **kwargs):
         """扩展 dict 使其支持点号(.)访问"""
         super(NBunch, self).__init__(*args, **kwargs)
         self.__dict__ = self
+        self.__default = default
 
         # 处理嵌套
         for key in self:
             if isinstance(self[key], dict):
                 self[key] = NBunch(self[key])
+
+    def __getattr__(self, key):
+        """"""
+        return self.get(key, self.__default)
 
 
 nb = NBunch({'a': 1, 'b': {'c': 2}})
