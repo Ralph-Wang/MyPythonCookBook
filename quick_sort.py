6d0511983f47c7b29d72156520a3d9aedaba4497
Ralph-Wang <ralph_ld@aliyun.com>
2014-11-26 14:43:37 +0800

    void 0

--- quick_sort.py
+++ quick_sort.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+'''
+用来证明 python 列表推导的强大
+'''
 
 import random
 
@@ -8,17 +11,8 @@ import random
 def quicksort(lst):
     if len(lst) <= 1:
         return lst
-
-    less = []
-    greater = []
-    pivot = lst.pop()
-    for other in lst:
-        if other < pivot:
-            less.append(other)
-        else:
-            greater.append(other)
-
-    return quicksort(less) + [pivot] + quicksort(greater)
+    return quicksort([lt for lt in lst[1:] if lt < lst[0]]) + [lst[0]]\
+        + quicksort([gt for gt in lst[1:] if gt >= lst[0]])
 
 
 case = range(20)
