f547ed668044b886d2bdca578593ad8efd277b8a
Ralph-Wang <ralph_ld@aliyun.com>
2015-06-01 11:56:06 +0800

    用 __call__ 来解决参数和 path 同名问题

--- tricks/getattr.py
+++ tricks/getattr.py
@@ -25,6 +25,32 @@ class Url(object):
 # /the/path/to/get
 print Url().the.path.to.get
 
-
 # /:user/repos: user -> ralph
 print Url().user('ralph').repos
+
+
+# 用 __call__ 来解决同名参数问题
+class Url2(object):
+
+    def __init__(self, path=''):
+        self._path = path
+
+    def __getattr__(self, path):
+        return Url2('{0}/{1}'.format(self._path, path))
+
+    def __call__(self, path):
+        path_list = self._path.split('/')
+        path_list[-1] = path
+        self._path = '/'.join(path_list)
+        return self
+
+    def __repr__(self):
+        return self._path
+
+    __str__ = __repr__
+
+# /:user/repos: user -> ralph
+print Url2().user('ralph').repos
+
+# /user/1
+print Url2().user.repos
