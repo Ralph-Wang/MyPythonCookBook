f279848f6890d020c4041d3fbc4ef7a1af92fd98
Ralph-Wang <ralph_ld@aliyun.com>
2015-09-23 15:11:43 +0800

    items schema for array

--- modules/jsonschema/use_validator.py
+++ modules/jsonschema/use_validator.py
@@ -9,7 +9,20 @@ schema = {
         "additionalProperties": False,
         "properties":{
             "price": {"type": "number"},
-            "name": {"type": "string"}
+            "name": {"type": "string"},
+            "tags" : {
+                "type": "array",
+                "items": {
+                    "type": "object",
+                    "required": ["name"], # draft 4
+                    "properties": {
+                        "name":{
+                            "type": "string",
+                            "required": True # draft 3
+                            }
+                        }
+                    },
+                }
             }
         }
 
@@ -24,3 +37,9 @@ print validator.is_type(obj, "object")
 for error in validator.iter_errors(obj):
     print repr(error)
 
+
+print "-" * 50
+
+obj = {"name": "box", "price": 25, "tags": [{}]}
+for error in validator.iter_errors(obj):
+    print repr(error)
